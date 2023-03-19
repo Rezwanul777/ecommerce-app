042f8984f0b9eb15f525b12bed845a99eddaff9b
Rezwanu Haque Prince <rezwanul.haquebd777@gmail.com>
2023-03-20 01:02:23 +0600

    admin route created

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-const Spinner = () => {
-   const [count,setCount]=useState(5)
+const Spinner = ({path="login"}) => {
+   const [count,setCount]=useState(3)
    const navigate=useNavigate()
    const location=useLocation()
 
@@ -10,11 +10,11 @@ const Spinner = () => {
       const interval=setInterval(()=>{
          setCount((prevValue)=>--prevValue)
       },1000);
-      count===0 && navigate('/login',{
+      count===0 && navigate(`/${path}`,{
          state:location.pathname
       })
       return ()=>clearInterval(interval)
-   },[count,navigate,location])
+   },[count,navigate,location,path])
    return (
    <div className="d-flex flex-column justify-content-center align-items-center" style={{height:"100vh"}}
    >
--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import AdminMenu from '../../components/layout/AdminMenu';
 import Layout from '../../components/layout/Layout';
+import { useAuth } from '../../context/auth';
 
 const AdminDashboard = () => {
+   const[auth]=useAuth()
    return (
       <Layout>
-      <h1>Dashboard</h1>
+         <div className="container-fluid m-3 p-3 dashboard-menu">
+            <div className="row">
+               <div className="col-md-3">
+                     <AdminMenu/>
+               </div>
+               <div className="col-md-9">
+                  <div className="card w-75 p-3">
+                     <h4>Admin Name:{auth?.user?.name}</h4>
+                     <h4>Admin email:{auth?.user?.email}</h4>
+                     <h4>Admin phone:{auth?.user?.phone}</h4>
+             
+                  </div>
+               </div>
+            </div>
+         </div>
       </Layout>
    );
 };
